b96dadcafe7779641724731ee5b5d4605456cfe4
derek <lideyu888@163.com>
2021-11-09 23:11:28 +0800

    style(API): 格式化代码

--- scripts/utils/juejin-api.js
+++ scripts/utils/juejin-api.js
@@ -1,20 +1,20 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const env = require("./env");
 
 const headers = {
-  'content-type': 'application/json; charset=utf-8',
-  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-  'accept-encoding': 'gzip, deflate, br',
-  'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-  'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-  'sec-ch-ua-mobile': '?0',
-  referer: 'https://juejin.cn/',
-  accept: '*/*',
+  "content-type": "application/json; charset=utf-8",
+  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+  "sec-ch-ua": `"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"`,
+  "sec-ch-ua-mobile": "?0",
+  referer: "https://juejin.cn/",
+  accept: "*/*",
   cookie: env.COOKIE
 };
 
 module.exports = {
-  async fetch({ path, method }) {
+  async fetch({path, method}) {
     return fetch(path, {
       headers: headers,
       method: method,

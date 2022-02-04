6aae0680ffd8a781e7a51ce2ba54a5eb7eda58db
derek <lideyu888@163.com>
2022-02-04 20:06:21 +0800

    chore: 移除杂项

--- scripts/utils/env.js
+++
@@ -1,8 +0,0 @@
-const env = process.env || {};
-
-module.exports = {
-  EMAIL_USER: env.EMAIL_USER,
-  EMAIL_PASS: env.EMAIL_PASS,
-  EMAIL_TO: env.EMAIL_TO,
-  COOKIE: env.COOKIE
-};
--- scripts/utils/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  async wait(time = 0) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  },
-  randomRangeNumber(start = 500, end = 1000) {
-    return (Math.random() * (end - start) + start) >> 0;
-  }
-}

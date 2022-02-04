12a43bc664259bf4cc6ece526b184c9fa77a8a86
derek <lideyu888@163.com>
2022-02-04 17:34:06 +0800

    test: 添加测试

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
 const JuejinHelper = require('..');
 
-console.log(JuejinHelper);
+const mockCookie = `juejin-cookie`;
+
+async function run() {
+  const juejin = new JuejinHelper();
+  await juejin.login(mockCookie);
+  console.log(juejin.getUser());
+}
+
+run();

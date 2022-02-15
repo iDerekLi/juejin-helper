e42665c8d5f1ece4d72715ba16ca27f6295cbea6
Derek <lideyu888@163.com>
2022-02-15 09:38:05 +0800

    test: 测试growth

--- test/test.js
+++ test/test.js
@@ -5,7 +5,21 @@ const mockCookie = `juejin-cookie`;
 async function run() {
   const juejin = new JuejinHelper();
   await juejin.login(mockCookie);
-  console.log(juejin.getUser());
+  console.log(juejin.getUser().user_name);
+
+  const growth = juejin.growth();
+
+  const luckyusersResult = await growth.getLotteriesLuckyUsers();
+  if (luckyusersResult.count > 0) {
+    const no1LuckyUser = luckyusersResult.lotteries[0];
+    const dipLuckyResult = await growth.dipLucky(no1LuckyUser.history_id);
+    console.log(dipLuckyResult);
+    if (dipLuckyResult.has_dip) {
+      console.log(`今天你已经沾过喜气，明天再来吧!`);
+    } else {
+      console.log(`沾喜气 +${dipLuckyResult.dip_value} 幸运值`);
+    }
+  }
 }
 
 run();

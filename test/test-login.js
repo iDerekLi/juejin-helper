33662afa550743e33a4b91e0c136b38733e0ee43
Derek <lideyu888@163.com>
2022-03-17 10:29:29 +0800

    test(test): 测试登录

--- test/test-login.js
+++ test/test-login.js
@@ -6,7 +6,6 @@ async function run() {
   const juejin = new JuejinHelper();
   await juejin.login(mockCookie);
   console.log("user_id", juejin.getUser().user_id);
-  console.log("tokens", juejin.getCookieTokens());
 }
 
 run();

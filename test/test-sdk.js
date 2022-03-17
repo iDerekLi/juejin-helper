9dd9df91f580a3654e6c3ca6160fa57f4f390058
Derek <lideyu888@163.com>
2022-03-17 10:30:02 +0800

    test(test): 测试sdk

---
+++ test/test-sdk.js
@@ -0,0 +1,16 @@
+const JuejinHelper = require('..');
+
+const mockCookie = `juejin-cookies`;
+
+async function run() {
+  const juejin = new JuejinHelper();
+  await juejin.login(mockCookie);
+  console.log("user_id", juejin.getUser().user_id);
+
+  const sdk = juejin.sdk();
+  console.log("SDK设置", await sdk.slardarSDKSetting());
+  console.log("成长API事件埋点", await sdk.mockTrackGrowthEvent());
+  console.log("load事件埋点", await sdk.mockTrackOnloadEvent());
+}
+
+run();

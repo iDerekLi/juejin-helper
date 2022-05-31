293b7454127c49d86387d9c28a062ff880bddb0e
ZiuChen <457353192@qq.com>
2022-05-31 13:15:20 +0800

    chore: 移除main_handler多余内容

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const checkin = require('./juejin-helper/scripts/checkin')
 const seaGold = require('./juejin-helper/scripts/seaGold')
-// const lottery = require('./juejin-helper/scripts/lottery')
 
 exports.main_handler = async () => {
-    return await Promise.all([checkin(), seaGold()])
-    // await lottery() 暂未开发
+    return Promise.all([checkin(), seaGold()])
 };
 
 main()

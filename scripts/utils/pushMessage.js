8c35db843001bc8feec0cc49b536121401d27ce5
Derek <lideyu888@163.com>
2022-05-31 10:59:29 +0800

    fix(push): 修复钉钉缺失依赖

--- scripts/utils/pushMessage.js
+++ scripts/utils/pushMessage.js
@@ -1,12 +1,12 @@
 const env = require("./env");
 const email = require("./email");
-const dingding = require("./dingding");
 const pushplus = require("./pushplus");
+const dingding = require("./dingding");
 
-async function main({ subject, text, html }) {
-  env.EMAIL_USER && email({ subject, text, html });
-  env.PUSHPLUS_TOKEN && pushplus({ subject, text, html });
-  env.DINGDING_WEBHOOK && dingding(text);
+async function pushMessage({ subject, text, html }) {
+  env.EMAIL_USER && await email({ subject, text, html });
+  env.DINGDING_WEBHOOK && await dingding({ subject, text, html });
+  env.PUSHPLUS_TOKEN && await pushplus({ subject, text, html });
 }
 
-module.exports = main;
+module.exports = pushMessage;

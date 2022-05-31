880e380cf8eeb920e44c188336cf9e6bb2cc7864
ZiuChen <457353192@qq.com>
2022-05-31 13:28:39 +0800

    fix: update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const checkin = require('./scripts/checkin')
 const seaGold = require('./scripts/seaGold')
 
 exports.main_handler = async () => {
-    return Promise.all([checkin(), seaGold()])
+    await checkin()
+    await seaGold()
 };
